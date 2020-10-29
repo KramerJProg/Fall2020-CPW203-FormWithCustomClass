2730c714ae4ac855530c0dfe2d285e246cd93ae3
Kramer Jensen <Kramerjensend@gmail.com>
2020-10-28 23:21:09 -0700

    Implemented VideoGame class and getVideoGame

---
+++ ts/main.ts
@@ -0,0 +1,73 @@
+class VideoGame {
+    title:string;
+    price:number;
+    rating:string;
+    isDigitalOnly:boolean;
+}
+
+// Test code
+/*
+let myGame = new VideoGame();
+myGame.title = "Mario";
+myGame.rating = "E";
+myGame.isDigitalOnly = true;
+*/
+
+window.onload = function() {
+    let addBtn = <HTMLElement> document.querySelector("input[type=button]");
+    addBtn.onclick = addVideoGame;
+}
+
+function addVideoGame() {
+    if (isAllDataValid()) {
+        let game = getVideoGame();
+        displayGame(game);
+    }
+}
+
+// Helper Method
+function getById(id:string) {
+    return document.getElementById(id);
+}
+
+/**
+ * Gets all game data from the form
+ * and returns it in a video game object
+ */
+function getVideoGame():VideoGame {
+    // Create game (Done)
+    let game = new VideoGame();
+    // Populate with data from the form
+    let titleInput = <HTMLInputElement>getById("title");
+    game.title = titleInput.value;
+
+    let priceInput = <HTMLInputElement>getById("price");
+    game.price = parseFloat(priceInput.value);
+
+    let ratingInput = <HTMLSelectElement>getById("rating");
+    game.rating = ratingInput.value;
+
+    let digitalOnly = <HTMLInputElement>getById("online");
+    game.isDigitalOnly = digitalOnly.checked;
+    // if (digitalOnly.checked) {
+    //     game.isDigitalOnly = true;
+    // }
+    // else {
+    //     game.isDigitalOnly = false;
+    // }
+
+    console.log(game);
+    // Return game
+    return game;
+
+}
+
+function displayGame(myGame:VideoGame):void {
+    // TODO: Display video game below the form
+
+}
+
+// ADD VALIDATION CODE
+function isAllDataValid() {
+    return true;
+}
